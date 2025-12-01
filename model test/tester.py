d8cf2c96832a088aa87a2a59a9bec1f0a7a9724f
Sergey Gladkikh <s.gladkikh1@g.nsu.ru>
2025-12-01 12:39:23 +0700

    Убрал лишнюю нормализацию

--- model test/tester.py
+++ model test/tester.py
@@ -4,9 +4,14 @@ import tensorflow as tf
 import keras
 IMG_SIZE = 224
 model= keras.models.load_model("trained.keras")
-img = cv.imread("mask.jpeg")
+stream = cv.VideoCapture(0)
+#img = cv.imread("mask.jpeg")
+ret, img=stream.read()
+cv.imshow("cam",img)
 img=cv.cvtColor(img,cv.COLOR_BGR2RGB)
 img=tf.image.resize(img,(IMG_SIZE,IMG_SIZE))
 img = tf.expand_dims(img, axis=0)  # Shape becomes (1, 224, 224, 3)
-img = tf.cast(img, tf.float32) / 255.0  # Normalize to [0, 1]
-print(model.predict(img))
+img = tf.cast(img, tf.float32)  
+
+print(np.argmax(model.predict(img)))
+cv.waitKey(0)
